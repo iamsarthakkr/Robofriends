4c3ff050e74d32cccc5544a942979c4a33a06f58
imasarthakkr <sarthakkumar10151@gmail.com>
2020-07-18 18:51:05 +0530

    first react component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Hello from "./Hello";
 import * as serviceWorker from "./serviceWorker";
+//importing tacyons
+import "tachyons";
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    {/* adding new component */}
+    <Hello greeting={"Hello React Ninja!"} />
   </React.StrictMode>,
   document.getElementById("root")
 );
