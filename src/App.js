be88b27fd2cfa626f9ed0b90cb36bf06ba5bf2e7
imasarthakkr <sarthakkumar10151@gmail.com>
2020-07-19 23:06:31 +0530

    Completed app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import React, { Component } from "react";
-import CardList from "./CardList";
-import { robots } from "./robots";
-import SearchBox from "./SearchBox";
+import React, { Component } from 'react';
+import CardList from './CardList';
+import './App.css';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: robots,
-      searchField: "",
-    };
-  }
-  onSearchChange = (event) => {
-    //update state
-    this.setState({ searchField: event.target.value });
-  };
+   constructor() {
+      super();
+      this.state = {
+         robots: robots,
+         searchField: '',
+      };
+   }
 
-  render() {
-    const filteredRobots = this.state.robots.filter(
-      (robot) => {
-        return robot.name
-          .toLowerCase()
-          .includes(this.state.searchField.toLowerCase());
-      }
-    );
-    return (
-      <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
-  }
+   onSearchChange = (event) => {
+      //update state
+      this.setState({ searchField: event.target.value });
+   };
+
+   render() {
+      const filteredRobots = this.state.robots.filter(
+         (robot) => {
+            return robot.name
+               .toLowerCase()
+               .includes(
+                  this.state.searchField.toLowerCase()
+               );
+         }
+      );
+      return (
+         <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange} />
+            <CardList robots={filteredRobots} />
+         </div>
+      );
+   }
 }
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
 const SearchBox = ({ searchChange, searchBox }) => {
-  return (
-    <div>
-      <input
-        className="pa3 ba b--green bg-lightest-blue"
-        type="search"
-        placeholder="search robots"
-        onChange={searchChange}
-      ></input>
-    </div>
-  );
+    return (
+        <div>
+            <input
+                className='pa3 ba b--green bg-lightest-blue'
+                type='search'
+                placeholder='search robots'
+                onChange={searchChange}
+            ></input>
+        </div>
+    );
 };
 
 export default SearchBox;
